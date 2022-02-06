b8804ad6c1bbed0a4cf54f5ca0e2afa56eff7e98
Stavros Kounis <skounis@gmail.com>
2022-02-06 18:53:12 +0100

    feat(module) Convert to ES.

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-// TODO: Conver to ES module (?)
-//       https://blog.logrocket.com/commonjs-vs-es-modules-node-js/
-exports.merci = function () {
+// ES module
+// https://blog.logrocket.com/commonjs-vs-es-modules-node-js/
+export function merci () {
   console.log("So Long, and Thanks for All the Fish");
 }
