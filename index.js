d340aefd2e5f53c1bee074a65084cba173bea22a
Stavros Kounis <skounis@gmail.com>
2022-02-06 18:39:21 +0100

    Initial commit.

---
+++ index.js
@@ -0,0 +1,5 @@
+// TODO: Conver to ES module (?)
+//       https://blog.logrocket.com/commonjs-vs-es-modules-node-js/
+exports.merci = function () {
+  console.log("So Long, and Thanks for All the Fish");
+}
